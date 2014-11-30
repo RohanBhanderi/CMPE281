94c60ae15f85c257ec6a8b5ea9c97a38625c0b13
Rohan Bhanderi <rohan.bhanderi@sjsu.edu>
2014-11-30 03:53:21 +0000

    Final changes

--- GMNodeClient/server.js
+++ GMNodeClient/server.js
@@ -2,6 +2,7 @@
 //  OpenShift sample Node application
 var express = require('express');
 var fs      = require('fs');
+var bodyParser = require('body-parser');
 
 // REST Client
 var RestClient = require('node-rest-client').Client;
@@ -25,8 +26,9 @@ var SampleApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        // For Cloud 9 process.env.PORT, process.env.IP
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || process.env.IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
@@ -35,27 +37,6 @@ var SampleApp = function() {
             self.ipaddress = "127.0.0.1";
         };
     };
-
-
-    /**
-     *  Populate the cache.
-     */
-    self.populateCache = function() {
-        if (typeof self.zcache === "undefined") {
-            self.zcache = { 'index.html': '' };
-        }
-
-        //  Local cache for static content.
-        self.zcache['index.html'] = fs.readFileSync('./index.html');
-    };
-
-
-    /**
-     *  Retrieve entry (content) from cache.
-     *  @param {string} key  Key identifying content to retrieve from cache.
-     */
-    self.cache_get = function(key) { return self.zcache[key]; };
-
 
     /**
      *  terminator === the termination handler
@@ -103,9 +84,9 @@ var SampleApp = function() {
             res.send("<html><body><img src='" + link + "'></body></html>");
         };
 
-        self.routes['/'] = function(req, res) {
+        self.routes['/openshift'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
-            res.send(self.cache_get('index.html') );
+			res.render('index.ejs');
         };
         
         self.routes['/test'] = function(req,res){
@@ -125,8 +106,148 @@ var SampleApp = function() {
                 res.end('Hello REST Client! Count Gumballs ==> ' + count );
             });
         };
+        
+        self.routes['/'] = function(req, res) {
+			restClient.get("http://grails-gumball-machine.cfapps.io/gumballs", function(data, response_raw){
+                // parsed response body as js object
+                console.log("DATA : " + data);
+                // raw response
+                console.log("response_raw:" + response_raw);
+                //console.log(data[0].id) ;
+                console.log(data[0].countGumballs) ;
+                console.log(data[0].modelNumber) ;
+                console.log(data[0].serialNumber) ;
+                var count =  data[0].countGumballs ;
+                var modelNumber = data[0].modelNumber ;
+				var serialNumber = data[0].serialNumber ;
+				var state = "NoCoinState";
+                console.log(count) ;
+				var msg = "--------------------------------------------------"
+						+"\nMighty Gumball, Inc."
+						+"\nGroovy-Enabled Standing Gumball"
+						+"\nModel# " + modelNumber
+						+"\nSerial# " + serialNumber 
+						+"\n--------------------------------------------------"
+						+"\nGumball Machine: "
+						+"\nCurrent State: " + state ;
+                var resData = {
+                    "msg":msg,
+					"model":modelNumber,
+					"serial":serialNumber,
+					"state":state
+                };							
+                res.setHeader('Content-Type', 'text/html');
+				res.render('gumball.ejs',resData);
+            });
+		
+            
+        };
+        
     };
 
+    self.createPostRoutes = function() {
+        
+        self.postRoutes = { };
+        
+        self.postRoutes['/gumball'] = function(req, res) {
+            //console.log(req.body.event);
+			
+            var action = req.body.event;
+            var state = req.body.state;
+			var modelNumber = req.body.model;
+			var serialNumber = req.body.serial;
+			
+			console.log(action);
+			console.log(state);
+			console.log(modelNumber);
+			console.log(serialNumber);
+			
+			if(action == 'Insert Quarter') {
+				if(state == 'NoCoinState' || state == 'CoinAcceptedState' || state == 'CoinRejectedState')
+					state = 'HasCoinState';
+			} else if (action == 'Turn Crank') {
+				if(state == 'HasCoinState')
+					state = 'CoinAcceptedState'; 
+				else if (state == 'CoinAcceptedState' || state == 'CoinRejectedState')
+					state = 'NoCoinState';
+			}
+			
+			if (state == 'CoinAcceptedState') {
+				restClient.get("http://grails-gumball-machine.cfapps.io/gumballs", function(data, response_raw){
+					// parsed response body as js object
+					console.log("DATA : " + data);
+					// raw response
+					//console.log("response_raw:" + response_raw);
+					//console.log(data[0].id) ;
+					console.log(data[0].countGumballs) ;
+					console.log(data[0].modelNumber) ;
+					console.log(data[0].serialNumber) ;
+					var count =  data[0].countGumballs ;
+					modelNumber = data[0].modelNumber ;
+					serialNumber = data[0].serialNumber ;
+					
+					console.log(count) ;
+					var msg = "";
+					if (count>0)
+					{	
+					count = count - 1;
+					
+					var args = {
+									data: {
+										"id": 1,
+										"countGumballs": count
+									},
+									headers:{"Content-Type": "application/json"}
+								};
+					
+					restClient.put("http://grails-gumball-machinelcknslcn.cfapps.io/gumballs/1", args, function(data,response) {
+						modelNumber = data.modelNumber ;
+					    serialNumber = data.serialNumber ;
+					});
+					
+					
+					msg = "--------------------------------------------------"
+							+"\nMighty Gumball, Inc."
+							+"\nGroovy-Enabled Standing Gumball"
+							+"\nModel# " + modelNumber
+							+"\nSerial# " + serialNumber 
+							+"\n--------------------------------------------------"
+							+"\nGumball Machine: "
+							+"\nCurrent State: " + state ;
+					} else {
+						msg = "Sorry We are out of gumballs !!"
+					}
+					var resData = {
+						"msg":msg,
+						"model":modelNumber,
+						"serial":serialNumber,
+						"state":state
+					};							
+					res.setHeader('Content-Type', 'text/html');
+					res.render('gumball.ejs',resData);
+				});
+				
+			} else {
+				var msg = "--------------------------------------------------"
+							+"\nMighty Gumball, Inc."
+							+"\nGroovy-Enabled Standing Gumball"
+							+"\nModel# " + modelNumber
+							+"\nSerial# " + serialNumber 
+							+"\n--------------------------------------------------"
+							+"\nGumball Machine: "
+							+"\nCurrent State: " + state ;
+					var resData = {
+						"msg":msg,
+						"model":modelNumber,
+						"serial":serialNumber,
+						"state":state
+					};							
+					res.setHeader('Content-Type', 'text/html');
+					res.render('gumball.ejs',resData);
+			}
+		};
+    }   
+    
 
     /**
      *  Initialize the server (express) and create the routes and register
@@ -134,11 +255,22 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         self.createRoutes();
+		self.createPostRoutes();
         self.app = express();
-
+		
+		self.app.set ('views', __dirname + '/views');
+		self.app.set ('view engine', 'ejs');
+		
+        self.app.use(express.bodyParser());
+        self.app.use("/images", express.static(__dirname + '/images'));
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
             self.app.get(r, self.routes[r]);
+        }
+		
+		//  Add handlers for the app (from the routes).
+        for (var r in self.postRoutes) {
+            self.app.post(r, self.postRoutes[r]);
         }
     };
 
@@ -148,7 +280,6 @@ var SampleApp = function() {
      */
     self.initialize = function() {
         self.setupVariables();
-        self.populateCache();
         self.setupTerminationHandlers();
 
         // Create the express server and routes.
@@ -169,12 +300,9 @@ var SampleApp = function() {
 
 };   /*  Sample Application.  */
 
-
-
 /**
  *  main():  Main code.
  */
 var zapp = new SampleApp();
 zapp.initialize();
 zapp.start();
-
